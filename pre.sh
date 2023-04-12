6af9dc217a094b716ce3544751ba8d0c66a542eb
YixuanDu <duyixuan_seu@foxmail.com>
2023-04-12 21:22:08 +0800

    update the readme

---
+++ pre.sh
@@ -0,0 +1,7 @@
+echo "# cmu15-445" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/WellYixuanDu/cmu15-445.git
+git push -u origin main
